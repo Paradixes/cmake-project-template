a6ced70f6e7670dc4843882f125024c37fa74481
Paradixes <ParadixeChen@outlook.com>
2024-04-18 01:42:18 +0800

    feat: add test case

---
+++ tests/includes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <catch2/catch_all.hpp>
+#include <chrono>
+#include <cstdio>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
+
+#include "my_project/project_interface.h"
---
+++ tests/test_case_1.cpp
@@ -0,0 +1,3 @@
+#include "includes.h"
+
+TEST_CASE("Test case 1", "[test_case_1]") { REQUIRE(1 == 1); }
