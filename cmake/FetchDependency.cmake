a91f1c27a30deabfcdec5e26de882b6b4b8d1b8d
Paradixes <2583013066@qq.com>
2024-04-18 01:56:42 +0800

    fix: remove googletest

--- cmake/FetchDependency.cmake
+++ cmake/FetchDependency.cmake
@@ -1,11 +1,7 @@
 include(FetchContent)
 
-FetchContent_Declare(googletest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG release-1.12.1)
-
 FetchContent_Declare(spdlog
         GIT_REPOSITORY https://github.com/gabime/spdlog.git
         GIT_TAG v1.13.0)
 
-FetchContent_MakeAvailable(googletest spdlog)
+FetchContent_MakeAvailable(spdlog)
